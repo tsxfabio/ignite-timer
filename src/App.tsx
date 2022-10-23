34e9814d0e2b3035d9b7d5ed2e4192c15fca5a29
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-23 03:08:47 -0300

    resolvendo conflitos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
-<<<<<<< HEAD
 import { GlobalStyle } from "./styles/global";
-=======
-import { GlobalStyles } from "./styles/global";
->>>>>>> 3e98469ab2a57eb847f848e7bdf61007fa8cd8f8
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
@@ -15,12 +11,8 @@ export function App() {
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
-<<<<<<< HEAD
 
       <GlobalStyle />
-=======
-      <GlobalStyles />
->>>>>>> 3e98469ab2a57eb847f848e7bdf61007fa8cd8f8
     </ThemeProvider>
   );
 }
