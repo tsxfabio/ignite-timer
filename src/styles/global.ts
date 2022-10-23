64630a0a4b50cef2fadcc8d89a7a29c6addb9144
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-23 03:10:28 -0300

    resolvendo conflitos

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-
-<<<<<<< HEAD
 export const GlobalStyle = createGlobalStyle`
 
 * {
@@ -10,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
 }
 
 body {
-    background-color: ${(props) => props.theme["gray-900"]};
+    background: ${(props) => props.theme["gray-900"]};
     color: #fff;
 }
 
@@ -20,18 +18,4 @@ body, input, textarea, button {
     font-size: 1rem;
 }
 
-=======
-export const GlobalStyles = createGlobalStyle`
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box; 
-}
-
-body {
-    background-color: #333;
-    color: #fff;
-}
-
->>>>>>> 3e98469ab2a57eb847f848e7bdf61007fa8cd8f8
 `;
