bba607637fc2e1740dd5e38ca1fc3a03f17e068b
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-24 23:11:41 -0300

    att formatador padrao

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
 
 * {
@@ -8,8 +8,8 @@ export const GlobalStyle = createGlobalStyle`
 }
 
 body {
-    background: ${(props) => props.theme["gray-900"]};
-    color: ${(props) => props.theme["white"]};
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme.white};
 }
 
 body, input, textarea, button {
@@ -18,4 +18,4 @@ body, input, textarea, button {
     font-size: 1rem;
 }
 
-`;
+`
