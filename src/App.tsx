8e78dc0cfc93e6f06454d802f610e73eb2840f79
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-26 00:02:30 -0300

    ajuste formatação ESlint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
+      <h1>Hello World</h1>
+      <h1>aaaaaaaa</h1>
       <GlobalStyle />
     </ThemeProvider>
   )
