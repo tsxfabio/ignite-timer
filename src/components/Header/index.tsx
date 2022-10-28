32a066ab6c87f35ee8ae330194a46360d6f563ec
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-28 02:02:36 -0300

    finalizando o estilo do defaultlayout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable prettier/prettier */
-import { HeaderContainer } from './styles'
+import { HeaderContainer } from "./styles";
+import { Timer, Scroll } from "phosphor-react";
+
+import logoIgnite from "../../assets/images/logo-ignite.svg";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
-  return (<HeaderContainer>
-    <h1>Logo</h1>
-    <nav>
-        <a href='#'>timer</a>
-        <a href='#'>history</a>
-    </nav>
-
-  </HeaderContainer>)
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} />
+      <nav>
+        <NavLink to='/' title='Timer'>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to='/history' title='Histórico'>
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 }
