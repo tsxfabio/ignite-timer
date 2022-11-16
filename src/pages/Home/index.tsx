2b1962f68601430b9be6dccc4c402e27ed375340
Fabio Leardini <leardinifabio90@gmail.com>
2022-11-15 21:39:53 -0300

    bugfix e timer no titulo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,13 +54,19 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         );
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   // Transforma o input 'task' em controlled input.
@@ -80,7 +86,7 @@ export function Home() {
     };
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
-
+    setAmountSecondPassed(0);
     reset();
   }
 
@@ -94,6 +100,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   return (
     <HomeContainer>
